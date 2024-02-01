b9b66ab445174ce3a6be8edfaefff529b3a5dd64
SirrahErydya <bo231@stud.uni-heidelberg.de>
2024-02-01 15:13:58 +0100

    On a long road to debug

--- src/som_viewer.py
+++ src/som_viewer.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 from astropy.visualization.wcsaxes import WCSAxesSubplot
 from glue.viewers.common.layer_artist import LayerArtist
+from glue.viewers.common.state import ViewerState
 from glue.viewers.image.viewer import MatplotlibImageMixin
 from glue.viewers.matplotlib.viewer import MatplotlibViewerMixin
 
@@ -19,7 +20,7 @@ from glue.viewers.image.layer_artist import ImageLayerArtist
 from glue.viewers.image.state import ImageViewerState, ImageLayerState
 from glue_qt.viewers.image.data_viewer import ImageViewer
 from glue_qt.viewers.matplotlib.widget import MplWidget
-from glue.viewers.image.frb_artist import imshow
+from glue.viewers.image.frb_artist import imshow, FRBArtist
 from glue.viewers.image.composite_array import CompositeArray
 
 from glue_qt.utils import load_ui
@@ -32,24 +33,41 @@ from matplotlib.image import AxesImage
 class SOMViewerState(ImageViewerState):
     def __init__(self, **kwargs):
         super(SOMViewerState, self).__init__(**kwargs)
+        self.color_mode = 'Colormaps'
 
 
 class SOMLayerState(ImageLayerState):
-    def __init__(self):
-        super(SOMLayerState, self).__init__()
-
+    def __init__(self, *args, **kwargs):
+        super(SOMLayerState, self).__init__(*args, **kwargs)
+        self.cmap = plt.colormaps.get_cmap('viridis')
 
 class SOMLayerArtist(LayerArtist):
 
-    _layer_state_cls = ImageLayerState
+    _layer_state_cls = SOMLayerState
 
     def __init__(self, axs, *args, **kwargs):
         super(SOMLayerArtist, self).__init__(*args, **kwargs)
         self.axs = axs
         self.images = []
         for i in range(len(self.axs)):
-            image = ImageLayerArtist(self.axs[i], *args, **kwargs)
+            image = ImageLayerArtist(self.axs[i], *args, layer_state=self.state)
             self.images.append(image)
+
+    def clear(self):
+        for img in self.images:
+            img.clear()
+
+    def remove(self):
+        for img in self.images:
+            img.remove()
+
+    def redraw(self):
+        for img in self.images:
+            img.redraw()
+
+    def update(self):
+        for img in self.images:
+            img.update()
 
 class SOMViewerStateWidget(QWidget):
 
@@ -95,8 +113,10 @@ class SOMDataViewer(ImageViewer):
     _data_artist_cls = SOMLayerArtist
     _subset_artist_cls = SOMLayerArtist
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, som_width, som_height, *args, **kwargs):
         super(SOMDataViewer, self).__init__(*args, **kwargs)
+        self.som_width = som_width
+        self.som_height = som_height
         self.mpl_widget = SOMWidget()
         self.setCentralWidget(self.mpl_widget)
         self.central_widget = self.mpl_widget
@@ -108,19 +128,10 @@ class SOMDataViewer(ImageViewer):
         self.resize(self.central_widget.size())
 
     def setup_callbacks(self):
-        self._wcs_set = False
-        self._changing_slice_requires_wcs_update = None
-        self.state.add_callback('x_att', self._set_wcs)
-        self.state.add_callback('y_att', self._set_wcs)
-        self.state.add_callback('slices', self._on_slice_change)
-        self.state.add_callback('reference_data', self._set_wcs, echo_old=True)
-
-        for i in range(len(self.axs)):
-            ax = self.axs[i]
+        for ax in self.axs:
             ax.set_adjustable('datalim')
             ax._composite = CompositeArray()
-            ax._composite_image = imshow(ax, ax._composite, aspect='auto',
-                                            origin='lower', interpolation='nearest')
+            ax._composite_image = imshow(ax, ax._composite, aspect='auto', origin='lower', interpolation='nearest', cmap='jet')
         self._set_wcs()
 
     def get_data_layer_artist(self, layer=None, layer_state=None):
@@ -131,11 +142,23 @@ class SOMDataViewer(ImageViewer):
 
     def create_plot(self):
         figure = self.central_widget.canvas.fig
-        for i in range(25):
-            axes = WCSAxesSubplot(figure, 5, 5, i+1)
+        for i in range(self.som_width*self.som_height):
+            axes = WCSAxesSubplot(figure, self.som_height, self.som_width, i+1)
             axes.axis('off')
             figure.add_axes(axes)
         plt.tight_layout()
         return figure
+
+    def _set_wcs(self, before=None, after=None, relim=True):
+        if hasattr(self, 'axs'):
+            self.axes = self.axs[2]
+            super()._set_wcs(before, None, relim)
+            self.axes = self.axs[8]
+            super()._set_wcs(before, None, relim)
+            #for ax in self.axs:
+                #self.axes = ax
+                #super()._set_wcs(before, after, relim)
+        else:
+            super()._set_wcs(before, after, relim)
 
 qt_client.add(SOMDataViewer)
--- src/visualization.py
+++ src/visualization.py
@@ -9,12 +9,15 @@ from src.som_viewer import SOMDataViewer
 def show_som(som, layout):
     som_dim = som.shape[0]
     neuron_dim = som.shape[2]
+    print(som_dim, neuron_dim)
     if layout == 'cartesian-2d':
         #img = Data(som=som.swapaxes(1, 2).reshape(som_dim*neuron_dim, som_dim*neuron_dim))
         img = Data(label="SOM prototypes", som=som)
         data_collection = DataCollection([img])
         app = GlueApplication(data_collection=data_collection)
-        viewer = app.new_data_viewer(SOMDataViewer)
+        viewer = SOMDataViewer(som_dim, som_dim, app.session)
+        viewer.register_to_hub(app.session.hub)
+        app.add_widget(viewer)
         viewer.add_data(img)
         app.start()
 
